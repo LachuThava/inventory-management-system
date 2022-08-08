92f0126d3c27da3f5f637ad071f450b46a661fe2
Lachu <tlaksman932@gmail.com>
2022-08-09 01:08:17 +0530

    student can assign and remove is working

--- src/Components/Lab_1.js
+++ src/Components/Lab_1.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { MinusCircleIcon, RefreshIcon, TagIcon} from '@heroicons/react/solid';
+import {TagIcon as UnTaggedIcon}  from '@heroicons/react/outline';
 import { auth, db} from '../firebase.js';
 import { collection, doc ,getDoc, onSnapshot, setDoc, updateDoc} from 'firebase/firestore';
 import { Button } from 'react-bootstrap';
@@ -7,6 +8,7 @@ import { Button } from 'react-bootstrap';
 import Request from './Request.js';
 import Popup from 'reactjs-popup';
 import PopupForm from './PopupForm.js';
+import InsertForm from './InsertForm.js';
 
 
 
@@ -14,7 +16,6 @@ import PopupForm from './PopupForm.js';
 const Lab_1 = () => {
     var[data,setData] = useState([]);
     var[idBoolean,setIdBoolean] = useState(false);
-    
 
     const checkRoleId = async()=>{
         const uid  = await auth.currentUser?.uid;
@@ -41,6 +42,10 @@ const Lab_1 = () => {
 
     }
 
+     async function handleTagCheck(){
+        const docRef = await doc(db,"Lending",)
+    }
+
     
 async function HandleRemove(props){
     console.log("props : ",props);
@@ -61,14 +66,41 @@ async function HandleRemove(props){
         await setDoc(docRef,{elec_json});
         console.log("update db");
 }
-    
+   async function handleRemoveTag(props){
+    // e.preventDefault();
+        const docRef1 = await doc(db,"lab_test","Lab_01");
+        const myDoc = await getDoc(docRef1);
+        console.log(props.id);
+        
+
+        var elec_json = [];
+        elec_json = myDoc.get("elec_json");
+        console.log("elec_json",elec_json);
+        elec_json.map((item,key)=>{
+            if(props===key){
+                console.log("working");
+                item['student'] = "";
+                item['received_time'] = "";
+                item['assigned_boolean'] = false;
+            }
+        })
+        console.log("after update : ",elec_json);
+
+
+        // console.log("elec_json : ",elec_json);
+        await setDoc(docRef1,{elec_json});
+    }
 
     function HandleTag(props){
         return (
-            <Popup trigger={<TagIcon className='h-5 w-5 mr-2 cursor-pointer  '/>}
+            <Popup trigger={<div>
+                            {props.tagbool &&<TagIcon onClick={() =>handleRemoveTag(props.id)} className='h-5 w-5 mr-2 cursor-pointer  '/>}
+                            {!props.tagbool && <UnTaggedIcon  className='h-5 w-5 mr-2 cursor-pointer  '/>}
+                    </div>}
             position='left bottom'
             >
-                <PopupForm id={props.id} />
+                {!props.tagbool && <PopupForm id={props.id} />}
+               {/* {props.tagbool && <RemoveForm id={props.id} />} */}
             </Popup>
         );
     }
@@ -89,7 +121,16 @@ async function HandleRemove(props){
 
 
 
-function handleInsert(){
+function HandleInsert(){
+    return(
+        <Popup trigger={<Button  className='rounded-lg p-2 w-36 bg-green-400 '>Insert</Button>}
+        position='right  bottom'
+        >
+            <InsertForm  />
+        </Popup>
+    );
+
+
 
 }
 if(data.length===0){
@@ -106,15 +147,15 @@ if(data.length===0){
               </div>
           </div>
           <div className='col flex mt-10 ml-3  '>
-              <Button className='rounded-lg p-2 w-36 bg-green-400 ' type='submit'>Insert</Button>
-              <Button className='rounded-lg p-2 ml-3 w-36 bg-green-400 ' type='submit'>Delete</Button>
+              < HandleInsert />
+              
           </div>
           <div className='flex -mt-5'>
               <div className=' flex font-bold w-screen  items-center justify-evenly mt-10 bg-slate-500 h-16 '>
                   <span className='w-16 col-3 text-center'>No.</span>
                   <span className='w-16 col-3 text-center'>Description</span>
                   <span className='w-16 col-2 text-center'>Name</span>
-                  <span className='w-16 col-4 text-center'>id</span>
+                  <span className='w-16 col-4 text-center'>Received_student</span>
               </div>
           </div>
           <div >
@@ -125,11 +166,11 @@ if(data.length===0){
                               <span className='w-36 col-4 text-center  bg-red-400'>{key}</span>
                               <span className='w-56 col-4 text-center bg-green-300'>{item.description}</span>
                               <span className='w-36 col-4 text-center bg-blue-300'>{item.name}</span>
-                              <span className='w-36 col-4 text-center  bg-yellow-300'>{item.id}</span>
+                              <span className='w-36 col-4 text-center  bg-yellow-300'>{item.student}</span>
                           </div>
                         {!idBoolean &&  <div className='flex items-center  w-1/12 justify-center '>
                               <RefreshIcon onChange={handleRepair} className='h-5 w-5 mr-2 cursor-pointer ' />
-                              <HandleTag id={key} />
+                              <HandleTag id={key} tagbool={item.assigned_boolean} />
                               <MinusCircleIcon onClick={() =>{HandleRemove(key)}} className='h-5 w-5 mr-2 cursor-pointer' />
                           </div>}
                       </div>
--- src/Components/PopupForm.js
+++ src/Components/PopupForm.js
@@ -1,12 +1,13 @@
 import {React,useState }from 'react';
-import {doc, getDoc, updateDoc } from 'firebase/firestore';
+import {doc, getDoc, query, serverTimestamp, setDoc, updateDoc, where } from 'firebase/firestore';
 import { Form,Button } from 'react-bootstrap';
 import { auth,db } from '../firebase';
+// import { isTagged, setIsTagged } from './Lab_1';
 
 const PopupForm= (props)=> {
 
-    let key = props.id.value;
-    console.log(props);
+    let key = props.id;
+    console.log(key);
 
     const[email,setEmail]=useState();
 
@@ -16,28 +17,49 @@ const PopupForm= (props)=> {
 
     async function handleAssign(e){
         e.preventDefault();
-        const docRef = await doc(db, 'Lending', "Lab_01");
+        // isTagged=true;
+        const docRef = await doc(db,'Lending',"Lab_01");
         const docSnap = getDoc(docRef);
         // console.log((await docSnap).get('temp_arr'));
         var temp_arr = [];
         temp_arr= (await docSnap).get('temp_arr');
         
-        let newDate = new Date()
+        let newDate = new Date();
         var temp_map = {
             "approved_by" : auth.currentUser?.email,
-            "asset_id": props.id,
-            "lab":"Lab_1",
+            "asset_id": props.id,   
             "student":email,
             "assigned_time": newDate.getFullYear()+"/"+newDate.getMonth()+"/"+newDate.getDate()+"   "+newDate.getHours()+":"+newDate.getMinutes()+":"+newDate.getSeconds()
         }
+        console.log(temp_map);  
         temp_arr.push(temp_map);
+       
+        await setDoc(docRef,
+            {temp_arr}
+        )
+        // console.log(temp_arr);
+
+        const docRef1 = await doc(db,"lab_test","Lab_01");
+        const myDoc = await getDoc(docRef1);
         
 
-            console.log(key);
-            await  updateDoc(docRef,
-                {temp_arr}
-            )
-        // }
+        var elec_json = [];
+        elec_json = myDoc.get("elec_json");
+        console.log("elec_json",elec_json);
+        elec_json.map((item,key)=>{
+            if(props.id===key){
+                console.log("working");
+                item['student'] = email;
+                item['received_time'] = newDate.getTime();
+                item['assigned_boolean'] = true;
+                // return {...item,student: email};  
+            }
+        })
+        console.log("after update : ",elec_json);
+
+
+        // console.log("elec_json : ",elec_json);
+        await setDoc(docRef1,{elec_json});
         console.log("function is working");
     }
 
